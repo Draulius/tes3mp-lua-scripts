bdd97010bb72a2d17735a755b1fa7c3ef6ccee9e
Draulius <joshua.draulius@gmail.com>
2026-06-18 00:11:14 -0500

    Update retroactiveHealth.lua

--- retroactiveHealth.lua
+++ retroactiveHealth.lua
@@ -5,7 +5,7 @@
 -- Staff characters are not corrected on login
 -- ====================== CONFIG =======================
 local allowHealthDecrease = false    -- If true, allows Hp to be corrected downwards (less safe for boosted characters).
-local applyLadyBonus      = false    -- If true, The Lady birthsign gives +25 Endurance to all health gains (from level 1 onward). Keep false for default TES3MP 0.8.1 behavior.
+local applyLadyBonus      = false    -- If true, The Lady birthsign gives +25 Endurance to all health gains. Keep false for default TES3MP 0.8.1 behavior.
 -- =====================================================
 --Do not edit below unless you know what you are doing
 

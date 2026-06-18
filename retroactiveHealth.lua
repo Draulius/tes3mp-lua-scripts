67dbe2465bdb458dcebb6dad4660bc70467f3fb5
Draulius <joshua.draulius@gmail.com>
2026-06-18 01:59:18 -0500

    Update retroactiveHealth.lua

--- retroactiveHealth.lua
+++ retroactiveHealth.lua
@@ -3,7 +3,7 @@
 -- Made by Lindran, originally for Mahkan Server, now released for public use
 -- Use or edit this script as you see fit, I just request that you give us credit
 -- ====================== CONFIG =======================
-local allowHealthDecrease = false    -- If true, allows HP to be corrected downwards (will affect boosted characters). Staff characters are not affected
+local allowHealthDecrease = false    -- If true, allows HP to be corrected downwards (so will affect boosted characters). Staff characters are not affected
 local applyLadyBonus      = false    -- If true, The Lady birthsign gives +25 Endurance to all health gains. Keep false for default TES3MP 0.8.1 behavior
 -- =====================================================
 -- Do not edit below unless you know what you are doing

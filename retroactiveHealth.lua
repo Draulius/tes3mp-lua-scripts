fd881cfa618343231ac019d4677963efb0d7389a
Draulius <joshua.draulius@gmail.com>
2026-06-18 17:29:05 +0000

    Update retroactiveHealth.lua
    commentary polish

--- retroactiveHealth.lua
+++ retroactiveHealth.lua
@@ -1,6 +1,6 @@
 -- Retroactive Health v1.04 - TES3MP 0.8.1
 -- Made by Lindran, originally for Mahkan Server, now released for public use.
--- This script gives you the full health bonus of Endurance retroactively, no matter how late you put points towards Endurance.
+-- This script gives you the full health bonus of Endurance retroactively, no matter how late you put points towards it.
 -- ====================== CONFIG =======================
 local allowHealthDecrease = false -- If true, allows HP to be corrected downwards (so will affect boosted characters). Staff characters are not affected.
 local applyLadyBonus = false -- If true, The Lady birthsign gives +25 Endurance to all health gains. Keep false for default TES3MP 0.8.1 behavior.
